d26bb8c2b302c2494a05f1ea8f11c1be4016e61c
ADARSH KUMAR DWIVEDI <67518067+01adarsh@users.noreply.github.com>
2022-06-02 23:59:46 +0530

    update app.py

--- app.py
+++ app.py
@@ -9,3 +9,10 @@ app.config['CDN_DOMAIN'] = 'cdn.yourdomain.com'
 #timestamp is by default set to True when using Flask-CDN. Therefore, it will be appended to the end of the asset's URL
 app.config["CDN_TIMESTAMP"] = False
 CDN(app)
+
+
+
+'''All static files associated with your Flask application should be located within a folder named static located in your Flask app's root directory. 
+Reference your static assets within your Flask templates using the following format ...'''
+
+#{{ url_for('static', filename='keycdn.png') }}
